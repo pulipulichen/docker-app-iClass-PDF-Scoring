967ef5e77d034d24cbe95bd810325e7b9a3d7307
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-11-25 14:57:34 +0800

    西元2023年11月25日 (週六) 14時57分34秒 CST

--- app/lib/GetExistedArgv.js
+++ app/lib/GetExistedArgv.js
@@ -2,14 +2,16 @@ const fs = require('fs')
 
 module.exports = function () {
   let args = process.argv
-  console.log({args})
+  // console.log({args})
   if (args.length < 3) {
     return []
   }
 
   args = args.slice(2)
   args = args.filter(arg => {
+    console.log(arg)
     return fs.existsSync(arg)
   })
+  console.log({args})
   return args
 }
