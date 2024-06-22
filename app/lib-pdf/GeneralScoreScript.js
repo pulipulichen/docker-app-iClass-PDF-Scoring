6c6dc229b19e4a71c1aac84ab60d5c55841a4cb3
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-06-22 12:27:39 +0800

    西元2024年06月22日 (週六) 12時27分39秒 CST

--- app/lib-pdf/GeneralScoreScript.js
+++ app/lib-pdf/GeneralScoreScript.js
@@ -10,26 +10,41 @@ function GeneralScoreScript(idTotalList, prependFilename) {
   let plusAdjTotalJS = {}
   let testJS = {}
 
+  let ratio110JS = {}
+  let ratio120JS = {}
+  let ratio130JS = {}
+
   // ----------------------------------------------------------------
 
-  idTotalList.forEach(({id, adjTotal, plusAdjTotal, comment}, i) => {
+  idTotalList.forEach(({id, adjTotal, plusAdjTotal, comment, ratio110_adj, ratio120_adj, ratio130_adj}, i) => {
     adjTotalJS[id] = {score: adjTotal, comment}
     plusAdjTotalJS[id] = {score: plusAdjTotal, comment}
 
     if (i < 3) {
       testJS[id] = {score: plusAdjTotal, comment}
     }
+
+    ratio110JS[id] = {score: ratio110_adj, comment}
+    ratio120JS[id] = {score: ratio120_adj, comment}
+    ratio130JS[id] = {score: ratio130_adj, comment}
   })
 
   adjTotalJS = createJS(adjTotalJS, prependFilename)
   plusAdjTotalJS = createJS(plusAdjTotalJS, prependFilename)
   testJS = createJS(testJS, prependFilename)
+
+  ratio110JS = createJS(ratio110JS, prependFilename)
+  ratio120JS = createJS(ratio120JS, prependFilename)
+  ratio130JS = createJS(ratio130JS, prependFilename)
   
   // ----------------------------------------------------------------
   return {
     adjTotalJS,
     plusAdjTotalJS,
-    testJS
+    testJS,
+    ratio110JS,
+    ratio120JS,
+    ratio130JS
   }
 }
 
